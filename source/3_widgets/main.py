036262750221f43edf406ed56550bd7538ee7154
Gabriel <g.alves.m.2008@gmail.com>
2020-05-31 19:34:26 -0300

    Add text input

--- source/3_widgets/main.py
+++ source/3_widgets/main.py
@@ -3,6 +3,7 @@ kivy.require('1.11.1')
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
 class WidgetsApp(App):
@@ -12,9 +13,10 @@ class WidgetsApp(App):
         print("The button was clicked!")
 
     def build(self):
+        ti = TextInput(text="Text input")
         bt = Button(text="Click here")
         bt.on_press = self.click
-        return bt
+        return ti
 
 
 if __name__ == '__main__':
