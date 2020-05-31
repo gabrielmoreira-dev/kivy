8f89fc01917c311543cf4619f746d0b2a0f5d307
Gabriel <g.alves.m.2008@gmail.com>
2020-05-31 20:00:45 -0300

    Add layout

--- source/3_widgets/main.py
+++ source/3_widgets/main.py
@@ -4,19 +4,38 @@ kivy.require('1.11.1')
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+Window.size = 500, 600
 
 
 class WidgetsApp(App):
 
     @staticmethod
     def click():
-        print("The button was clicked!")
+        print('Ok')
 
     def build(self):
-        ti = TextInput(text="Text input")
+        layout = FloatLayout()
+
+        ti = TextInput(text="Enter your text here")
+        ti.size_hint = None, None
+        ti.height = 300
+        ti.width = 400
+        ti.x = 60
+        ti.y = 250
+
         bt = Button(text="Click here")
         bt.on_press = self.click
-        return ti
+        bt.size_hint = None, None
+        bt.height = 50
+        bt.width = 200
+        bt.x = 170
+        bt.y = 150
+
+        layout.add_widget(ti)
+        layout.add_widget(bt)
+        return layout
 
 
 if __name__ == '__main__':
